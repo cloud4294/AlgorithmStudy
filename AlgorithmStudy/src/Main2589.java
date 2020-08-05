aad6ed5c0d1fc8e0c610058f469ca7c02558b0b1
cloud4294 <cloud4294@gmail.com>
2020-08-05 16:44:20 +0900

    2020-08-05

--- AlgorithmStudy/src/Main2589.java
+++ AlgorithmStudy/src/Main2589.java
@@ -4,88 +4,94 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+ * 		Baekjoon Online Judge 2589번 - 보물섬
+ * 
+ * 		https://www.acmicpc.net/problem/2589
+ */
+
 public class Main2589 {
 
-	static int N, M;
+	static int R, C, result;
 	static char[][] map;
-	static boolean[][] checked;
-	static int res;
-	static int[] my = { -1, 0, 1, 0 };
-	static int[] mx = { 0, 1, 0, -1 };
+	static int[][] group;
+	static int[] dr = { -1, 0, 1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
 
-	static class dot {
-		int y;
-		int x;
-		int length;
+	static class point {
 
-		public dot(int y, int x, int length) {
+		int r;
+		int c;
+		int count;
+
+		public point(int r, int c, int count) {
 			super();
-			this.y = y;
-			this.x = x;
-			this.length = length;
-		}
-	} // 좌표와 길이를 저장할 클래스 
-
-	public static void bfs(int i, int j) {
-		checked = new boolean[N + 2][M + 2];
-		Queue<dot> queue = new LinkedList<>();
-
-		dot start = new dot(i, j, 0);
-		queue.offer(start);
-		int l = 0;
-
-		while (!queue.isEmpty()) { // bfs시행
-			dot now = queue.poll();
-
-			if (now.length >= l) {
-				l = now.length;
-			}
-
-			for (int k = 0; k < 4; k++) {
-				int ny = now.y + my[k];
-				int nx = now.x + mx[k];
-
-				if (map[ny][nx] != 'L')
-					continue;
-
-				if (checked[ny][nx] == false) {
-					checked[ny][nx] = true;
-					queue.offer(new dot(ny, nx, now.length + 1));
-				}
-			}
+			this.r = r;
+			this.c = c;
+			this.count = count;
 		}
 
-		if (l > res) { // bfs종료시 길이가 현재 길이보다 길다면 갱신
-			res = l;
-		}
-
-	} 
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] rc = br.readLine().split(" ");
-		N = Integer.parseInt(rc[0]);
-		M = Integer.parseInt(rc[1]);
 
-		map = new char[N + 2][M + 2];
-
-		res = 0;
-
-		for (int i = 1; i <= N; i++) {
+		String[] size = br.readLine().split(" ");
+		R = Integer.parseInt(size[0]);
+		C = Integer.parseInt(size[1]);
+		map = new char[R][C];
+		group = new int[R][C];
+		result = 0;
+		for (int i = 0; i < R; i++) {
 			String line = br.readLine();
-			for (int j = 1; j <= M; j++) {
-				map[i][j] = line.charAt(j - 1);
+			for (int j = 0; j < C; j++) {
+				map[i][j] = line.charAt(j);
 			}
-		} // 입력값 초기화
+		}
 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= M; j++) {
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
 				if (map[i][j] == 'L')
-					bfs(i, j);
+					solve(i, j);
 			}
-		} // L값을 가진 모든 지점에 대해 bfs시행
-		System.out.println(res);
+		}
+		System.out.println(result);
+	}	
 
+	public static void solve(int i, int j) {
+		boolean[][] visit = new boolean[R][C];
+
+		point start = new point(i, j, 0);
+		visit[i][j] = true;
+
+		Queue<point> queue = new LinkedList<>();
+		queue.offer(start);
+		int length = 0;
+		while (!queue.isEmpty()) {
+			point now = queue.poll();
+
+			length = Math.max(length, now.count);
+
+			for (int k = 0; k < 4; k++) {
+				int nr = now.r + dr[k];
+				int nc = now.c + dc[k];
+
+				if (isRange(nr, nc) == false)
+					continue;
+
+				if (map[nr][nc] == 'L' && visit[nr][nc] == false) {
+					visit[nr][nc] = true;
+					queue.offer(new point(nr, nc, now.count + 1));
+				}
+			}
+		}
+		result = Math.max(result, length);
+	}
+
+	public static boolean isRange(int nr, int nc) {
+		if (nr < 0 || nr >= R || nc < 0 || nc >= C)
+			return false;
+		return true;
 	}
 
 }
